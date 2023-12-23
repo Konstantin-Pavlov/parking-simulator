19c2ecc9dccfef45b01ce4ba23a969d596f32761
Konstantin-Pavlov <konstpavlov87@gmail.com>
2023-12-23 12:01:24 +0600

    added custom exception

---
+++ src/main/java/customExceptions/IlligalDateException.java
@@ -0,0 +1,10 @@
+package customExceptions;
+
+public class IlligalDateException extends Exception{
+    public IlligalDateException() {
+    }
+
+    public IlligalDateException(String message) {
+        super(message);
+    }
+}
