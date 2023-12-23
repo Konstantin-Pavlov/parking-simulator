cb289a262d75bc144a59396f0f7ea7eb8fdcb3b7
Konstantin-Pavlov <konstpavlov87@gmail.com>
2023-12-23 12:03:24 +0600

    added check if entryTime is after exit time; added check if car was parked less then 30 minutes (in this case parking is free)

--- src/main/java/util/Biller.java
+++ src/main/java/util/Biller.java
@@ -1,7 +1,7 @@
 package util;
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
+import customExceptions.IlligalDateException;
+
 import java.time.Duration;
 import java.time.LocalDateTime;
 
@@ -9,13 +9,20 @@ public class Biller {
     private Biller() {
     }
 
-    public static double calculateBill(LocalDateTime entryTime, LocalDateTime exitTime){
+    public static double calculateBill(LocalDateTime entryTime, LocalDateTime exitTime) throws IlligalDateException {
         // todo use BigDecimal for cash ops
 //        BigDecimal value = new BigDecimal("123.456789");
 //        BigDecimal roundedValue = value.setScale(2, RoundingMode.HALF_UP);
 //        System.out.println(roundedValue); // Output: 123.46
 
-        //todo 30 mins is free
+        if (entryTime.isAfter(exitTime)) {
+            throw new IlligalDateException("the entry date can not be after exit date");
+        }
+
+        // 30 minutes is free
+        if (Duration.between(entryTime, exitTime).toMinutes() <= 30) {
+            return 0.0;
+        }
 
         double costPerFiveMinutes = 0.10;
         double totalCost = entryTime.getMinute() % 5 == 0 ? 0.0 : 0.1; // for example: if the car arrives at 15:57 we charge 5 minutes (15:55-16:00)
@@ -24,11 +31,11 @@ public class Biller {
         int endFreeParkingTime = 9;
 
         // maybe use it later
-        LocalDateTime freeParkingStart = LocalDateTime.of(entryTime.getYear(), entryTime.getMonth(), entryTime.getDayOfMonth(), 21, 0).minusDays(1);
-        LocalDateTime freeParkingEnd = LocalDateTime.of(exitTime.getYear(), exitTime.getMonth(), exitTime.getDayOfMonth(), 9, 0);
-
-        Duration parkingDuration = Duration.between(entryTime, exitTime);
-        long totalMinutes = parkingDuration.toMinutes();
+//        LocalDateTime freeParkingStart = LocalDateTime.of(entryTime.getYear(), entryTime.getMonth(), entryTime.getDayOfMonth(), 21, 0).minusDays(1);
+//        LocalDateTime freeParkingEnd = LocalDateTime.of(exitTime.getYear(), exitTime.getMonth(), exitTime.getDayOfMonth(), 9, 0);
+//
+//        Duration parkingDuration = Duration.between(entryTime, exitTime);
+//        long totalMinutes = parkingDuration.toMinutes();
 
         ////
 
@@ -37,7 +44,7 @@ public class Biller {
             if (currentMinute.getHour() == startFreeParkingTime) {
                 currentMinute = currentMinute.plusHours(12);
             } else {
-                if(currentMinute.getMinute() % 5 == 0){
+                if (currentMinute.getMinute() % 5 == 0) {
                     totalCost += costPerFiveMinutes;
                 }
                 currentMinute = currentMinute.plusMinutes(1);
