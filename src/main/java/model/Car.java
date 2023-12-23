ca626af120a4dde1467688541fdd726819c5d873
Konstantin-Pavlov <konstpavlov87@gmail.com>
2023-12-23 16:44:21 +0600

    added Car class

---
+++ src/main/java/model/Car.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Car {
+    private final String regNumber;
+
+    public Car(String regNumber) {
+        this.regNumber = regNumber;
+    }
+
+    public String getRegNumber() {
+        return regNumber;
+    }
+}
