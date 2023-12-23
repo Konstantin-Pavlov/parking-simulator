39831b0803e422849277926f4f8078151fe32393
Konstantin-Pavlov <konstpavlov87@gmail.com>
2023-12-23 17:00:59 +0600

    added methods hashcode, equals and toString

--- src/main/java/model/Car.java
+++ src/main/java/model/Car.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.Objects;
+
 public class Car {
     private final String regNumber;
 
@@ -9,5 +11,29 @@ public class Car {
 
     public String getRegNumber() {
         return regNumber;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(regNumber);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Car otherCar = (Car) obj;
+        return regNumber.equals(otherCar.regNumber);
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "regNumber='" + regNumber + '\'' +
+                '}';
     }
 }
