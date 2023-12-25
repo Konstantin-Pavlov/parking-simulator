d22e413b5a8e73ddbb4cc8ee582862bc461b1ca1
Konstantin-Pavlov <konstpavlov87@gmail.com>
2023-12-26 03:28:05 +0600

    some fields moved to ParkingRecord class to comply with solid principles

--- src/main/java/model/Check.java
+++ src/main/java/model/Check.java
@@ -4,51 +4,21 @@ import customExceptions.IlligalDateException;
 import util.Biller;
 
 import java.math.BigDecimal;
-import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 public class Check {
-    Car car;
-    private final LocalDateTime arrivalTime;
-    private final LocalDateTime departureTime;
-    private final Duration duration;
-    boolean valid;
-    BigDecimal bill;
+    // should be created in parking log? after car exits parking. so we have exit time and can generate a check
+    private final BigDecimal bill;
     private final UUID id;
 
-    public Check(Car car, LocalDateTime arrivalTime, LocalDateTime departureTime) throws IlligalDateException {
-        this.car = car;
-        this.arrivalTime = arrivalTime;
-        this.departureTime = departureTime;
-        this.duration = Duration.between(arrivalTime, departureTime);
-        this.valid = true;
+    public Check(LocalDateTime arrivalTime, LocalDateTime departureTime) throws IlligalDateException {
         this.bill = Biller.calculateBill(arrivalTime, departureTime);
         this.id = UUID.randomUUID(); // Generate a unique ID using UUID
     }
 
     public UUID getId() {
         return id;
-    }
-
-    public Car getCar() {
-        return car;
-    }
-
-    public LocalDateTime getArrivalTime() {
-        return arrivalTime;
-    }
-
-    public LocalDateTime getDepartureTime() {
-        return departureTime;
-    }
-
-    public Duration getDuration() {
-        return duration;
-    }
-
-    public boolean isValid() {
-        return valid;
     }
 
     public BigDecimal getBill() {
