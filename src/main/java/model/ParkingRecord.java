456b75f28d35a27065137f3bb937049374f79ee0
Konstantin-Pavlov <konstpavlov87@gmail.com>
2023-12-26 16:40:18 +0600

    add methods

--- src/main/java/model/ParkingRecord.java
+++ src/main/java/model/ParkingRecord.java
@@ -1,18 +1,19 @@
 package model;
 
+import customExceptions.IlligalDateException;
+
 import java.time.Duration;
 import java.time.LocalDateTime;
 
 public class ParkingRecord {
     private final  Car car; // maybe move to parking log
-    private boolean valid; // change to complete? flag that the record is done (car arrives, stays, pays the bill, leaves)
+    private boolean complete;
     private final LocalDateTime arrivalTime;
     private LocalDateTime departureTime;
-    private Duration duration;
 
     public ParkingRecord(Car car, LocalDateTime arrivalTime) {
         this.car = car;
-        this.valid = false; // until car leaves the parking and pays the bill
+        this.complete = false; // until car leaves the parking and pays the bill
         this.arrivalTime = arrivalTime;
 
         // we can't know departureTime and duration when car just arrives to the parking lot
@@ -21,15 +22,38 @@ public class ParkingRecord {
     }
 
     public void registerEntryToParking(){
-
+        // useless
     }
 
     public void registerLeavingTheParkingLot(LocalDateTime departureTime){
         this.departureTime = departureTime;
-        this.valid = true;
-        // generate bill?
-        // generate check?
+        this.complete = true;
+        // generate bill? and store it in this class
+        // generate check? and store it in this class
     }
 
+    public Car getCar() {
+        return car;
+    }
 
+    public LocalDateTime getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public LocalDateTime getDepartureTime() {
+        return departureTime;
+    }
+
+    public boolean isRecordComplete() {
+        return complete;
+    }
+
+    public Duration getDuration() throws IlligalDateException {
+        if (arrivalTime == null ) {
+            throw new  IlligalDateException("arrival time is null");
+        } else if(departureTime == null){
+            throw new IlligalDateException("departure time is null");
+        }
+        return Duration.between(arrivalTime, departureTime);
+    }
 }
