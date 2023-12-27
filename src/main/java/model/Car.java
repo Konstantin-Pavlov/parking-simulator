549fe6f2486511a9d296b6484f43c27c21adab20
Konstantin-Pavlov <konstpavlov87@gmail.com>
2023-12-28 01:52:32 +0600

    add methods that change car state and handle custom exceptions

--- src/main/java/model/Car.java
+++ src/main/java/model/Car.java
@@ -1,16 +1,66 @@
 package model;
 
+import carStates.OnRoute;
+import customExceptions.IlligalCarStateException;
+import interfaces.CarState;
+
 import java.util.Objects;
 
 public class Car {
     private final String regNumber;
 
+    private CarState state;
+    private String stateMessage;
+
     public Car(String regNumber) {
         this.regNumber = regNumber;
+        this.setState(new OnRoute());
+    }
+
+    public void park() {
+        try {
+            this.state.park(this);
+        } catch (IlligalCarStateException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void leave() {
+        try {
+            this.state.leaveParking(this);
+        } catch (IlligalCarStateException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     public String getRegNumber() {
         return regNumber;
+    }
+
+    public CarState getState() {
+        return state;
+    }
+
+    public void setState(CarState state) {
+        this.state = state;
+        switch (this.state.getClass().getSimpleName()) {
+            case "OnRoute":
+                setStateMessage("driving");
+                break;
+            case "OnPark":
+                setStateMessage("parked");
+                break;
+            default:
+                System.err.println("unknown state");
+        }
+    }
+
+    public String getStateMessage() {
+        return stateMessage;
+    }
+
+    public void setStateMessage(String stateMessage) {
+        this.stateMessage = stateMessage;
     }
 
     @Override
