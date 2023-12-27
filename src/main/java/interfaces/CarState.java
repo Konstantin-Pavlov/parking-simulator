a68a564c50eafddc0ead324218ebf3acf8d4c496
Konstantin-Pavlov <konstpavlov87@gmail.com>
2023-12-28 01:50:52 +0600

    now methods have an exceptions

--- src/main/java/interfaces/CarState.java
+++ src/main/java/interfaces/CarState.java
@@ -1,8 +1,9 @@
 package interfaces;
 
+import customExceptions.IlligalCarStateException;
 import model.Car;
 
 public interface CarState {
-    public void park(Car car);
-    public void leaveParking(Car car);
+    public void park(Car car) throws IlligalCarStateException;
+    public void leaveParking(Car car) throws IlligalCarStateException;
 }
